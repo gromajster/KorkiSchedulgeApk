a8a5e3cc8a7e8e06b4fd77fdc16f04a1f69f76ed
Wheezy <mateusz.wedel@gmail.com>
2019-06-05 23:05:34 +0200

    Add docker config.

--- src/main/java/pl/korkischedule/korki/KorkiApplication.java
+++ src/main/java/pl/korkischedule/korki/KorkiApplication.java
@@ -6,13 +6,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @EnableAsync
 @SpringBootApplication
 public class KorkiApplication {
 
 
-    public static void main(String[] args) {
-        System.getenv("JDBC_DATABASE_URL");
+    public static void main(String[] args) throws URISyntaxException
+    {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+    
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        
+        System.setProperty("JDBC_DATABASE_URL", dbUrl);
+        System.setProperty("JDBC_DATABASE_USERNAME", username);
+        System.setProperty("JDBC_DATABASE_PASSWORD", password);
+        
         SpringApplication.run(KorkiApplication.class, args);
     }
 
